84fc178901edebfc662abb0c2c891419644b9f17
Digital <josephpro42@live.co.uk>
2018-08-13 00:20:00 +0100

    Ignore maven files

--- bungeecord/src/main/java/net/sunken/bungeecord/BungeeMain.java
+++ bungeecord/src/main/java/net/sunken/bungeecord/BungeeMain.java
@@ -2,14 +2,26 @@ package net.sunken.bungeecord;
 
 import lombok.Getter;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.sunken.common.Common;
+import net.sunken.common.type.ServerType;
 
 public class BungeeMain extends Plugin {
 
     @Getter
     private static BungeeMain instance;
 
+    private static Common common;
+
     public void onEnable(){
         instance = this;
+
+        // Initialize common
+        common = new Common();
+        common.onCommonLoad(ServerType.BUNGEECORD);
+    }
+
+    public void onDisable(){
+        common.onCommonDisable();
     }
 
 }
