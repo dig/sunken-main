0bfe027eb4e2a2a70bfb6218162661355ffcb129
Digital <josephpro42@live.co.uk>
2018-08-13 18:50:18 +0100

    Rename bungee and reverse lobbies

--- bungeecord/src/main/java/net/sunken/bungeecord/lobby/LobbyHandler.java
+++ bungeecord/src/main/java/net/sunken/bungeecord/lobby/LobbyHandler.java
@@ -4,9 +4,11 @@ import net.sunken.common.Common;
 import net.sunken.common.lobby.LobbyInfo;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class LobbyHandler {
@@ -17,6 +19,8 @@ public class LobbyHandler {
         List<LobbyInfo> sortedViaPlayerCount = lobbies.stream()
                 .sorted(Comparator.comparing(LobbyInfo::getPlayerCount))
                 .collect(Collectors.toList());
+
+        Collections.reverse(sortedViaPlayerCount);
 
         double middle = ((double) lobbies.size()) / 2;
         int middleFloor = (int) middle;
