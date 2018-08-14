e3f0d1f25a78260c72594e8d29b9aa2ad17c6ffb
Digital <josephpro42@live.co.uk>
2018-08-14 17:45:40 +0100

    Start lobbyplayer and update bungee constants

--- bungeecord/src/main/java/net/sunken/bungeecord/Constants.java
+++ bungeecord/src/main/java/net/sunken/bungeecord/Constants.java
@@ -1,9 +1,11 @@
 package net.sunken.bungeecord;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Constants {
 
-    public static final String NO_LOBBY = "&cThere are currently no lobbies online!";
+    public static final String NO_LOBBY = "&cThere are no available lobbies!";
 
-    public static final String MOTD_TOP_LINE = "Sunken";
-    public static final String MOTD_BOTTOM_LINE = "Just testing k";
+    public static final String MOTD_TOP_LINE = ChatColor.BLUE + "&lSunken &7| " + ChatColor.AQUA + "&l1.13 SUPPORT";
+    public static final String MOTD_BOTTOM_LINE = "&a&lHOME OF &6&lUNIQUE &a&lMINIGAMES";
 }
--- lobby/src/main/java/net/sunken/lobby/listeners/PlayerListener.java
+++ lobby/src/main/java/net/sunken/lobby/listeners/PlayerListener.java
@@ -1,13 +1,12 @@
 package net.sunken.lobby.listeners;
 
-import com.mojang.datafixers.types.templates.Const;
-import net.sunken.core.util.ScoreboardUtil;
+import net.sunken.common.Common;
 import net.sunken.core.util.TabListUtil;
 import net.sunken.core.util.chat.MessageUtil;
 import net.sunken.lobby.Constants;
 import net.sunken.lobby.LobbyPlugin;
+import net.sunken.lobby.player.LobbyPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -15,16 +14,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        FileConfiguration config = LobbyPlugin.getInstance().getConfig();
         event.setJoinMessage("");
 
-        FileConfiguration config = LobbyPlugin.getInstance().getConfig();
+        // Add to hashmap
+        LobbyPlayer lobbyPlayer = new LobbyPlayer(player);
+        Common.getInstance().getOnlinePlayers().put(player.getUniqueId().toString(), lobbyPlayer);
 
+        // Set spawn
         Location spawn = new Location(
                 Bukkit.getWorld(config.getString("spawn.world")),
                 config.getDouble("spawn.x"),
@@ -45,5 +49,13 @@ public class PlayerListener implements Listener {
         for(int i = 0; i<Constants.JOIN_MESSAGES.size(); i++){
             player.sendMessage(MessageUtil.getCenteredMessage(Constants.JOIN_MESSAGES.get(i), MessageUtil.CENTER_PX));
         }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onQuit(PlayerQuitEvent event){
+        Player player = event.getPlayer();
+        event.setQuitMessage("");
+
+        Common.getInstance().getOnlinePlayers().remove(player.getUniqueId().toString());
     }
 }
