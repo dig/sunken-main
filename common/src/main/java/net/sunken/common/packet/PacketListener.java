df6302cfae1d6500f46f1928f31cefdefd7bafa7
Digital <josephpro42@live.co.uk>
2018-08-16 23:14:48 +0100

    Packet listener revert

--- common/src/main/java/net/sunken/common/packet/PacketListener.java
+++ common/src/main/java/net/sunken/common/packet/PacketListener.java
@@ -1,62 +1,61 @@
 package net.sunken.common.packet;
 
-import net.sunken.common.Common;
 import redis.clients.jedis.BinaryJedisPubSub;
 import redis.clients.jedis.Jedis;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.logging.Level;
 
-public class PacketListener extends BinaryJedisPubSub {
+public class PacketListener {
 
-    static final byte[] PACKET_CHANNEL = "SUNKEN_PACKET_CHANNEL".getBytes();
+    public static final byte[] PACKET_CHANNEL = "SUNKEN_PACKET_CHANNEL".getBytes();
 
     private final Jedis subscriberJedis;
-
-    private Map<Class<? extends Packet>, PacketHandler> handlers = PacketHandlerRegistry.getHandlers();
-
     public PacketListener(Jedis subscriberJedis) {
         this.subscriberJedis = subscriberJedis;
     }
 
     public void start() {
-        new Thread(() -> subscriberJedis.subscribe(this, PACKET_CHANNEL)).start();
+        new Thread(() -> {
+            subscriberJedis.subscribe(new Listener(), PACKET_CHANNEL);
+        }).start();
     }
 
-    @Override
-    public void onMessage(byte[] channel, byte[] message) {
-        Common.getLogger().log(Level.INFO, " onMessage() PacketListener");
+    private class Listener extends BinaryJedisPubSub {
 
-        if (Arrays.equals(channel, PACKET_CHANNEL)) {
-            Packet deserialized = Packet.fromBytes(message);
+        private Map<Class<? extends Packet>, PacketHandler> handlers = PacketHandlerRegistry.getHandlers();
 
-            Common.getLogger().log(Level.INFO, " onMessage() handler: " + deserialized.getClass());
+        @Override
+        public void onMessage(byte[] channel, byte[] message) {
+            if (Arrays.equals(channel, PACKET_CHANNEL)) {
+                Packet deserialized = Packet.fromBytes(message);
 
-            if (deserialized != null && handlers.containsKey(deserialized.getClass())) {
-                PacketHandler handler = handlers.get(deserialized.getClass());
-                handler.onReceive(deserialized);
+                if (deserialized != null && handlers.containsKey(deserialized.getClass())) {
+                    PacketHandler handler = handlers.get(deserialized.getClass());
+                    handler.onReceive(deserialized);
+                }
             }
+        }
+
+        @Override
+        public void onPMessage(byte[] pattern, byte[] channel, byte[] message) {
+        }
+
+        @Override
+        public void onSubscribe(byte[] channel, int subscribedChannels) {
+        }
+
+        @Override
+        public void onUnsubscribe(byte[] channel, int subscribedChannels) {
+        }
+
+        @Override
+        public void onPUnsubscribe(byte[] pattern, int subscribedChannels) {
+        }
+
+        @Override
+        public void onPSubscribe(byte[] pattern, int subscribedChannels) {
         }
     }
 
-    @Override
-    public void onPMessage(byte[] pattern, byte[] channel, byte[] message) {
-    }
-
-    @Override
-    public void onSubscribe(byte[] channel, int subscribedChannels) {
-    }
-
-    @Override
-    public void onUnsubscribe(byte[] channel, int subscribedChannels) {
-    }
-
-    @Override
-    public void onPUnsubscribe(byte[] pattern, int subscribedChannels) {
-    }
-
-    @Override
-    public void onPSubscribe(byte[] pattern, int subscribedChannels) {
-    }
 }
