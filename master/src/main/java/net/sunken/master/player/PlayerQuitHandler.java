dab37c072162a66db91eea5b34316bbbb8280006
Digital <josephpro42@live.co.uk>
2018-08-21 15:57:10 +0100

    Remove pending invites when they leave the server

--- master/src/main/java/net/sunken/master/player/PlayerQuitHandler.java
+++ master/src/main/java/net/sunken/master/player/PlayerQuitHandler.java
@@ -4,16 +4,23 @@ import net.sunken.common.Common;
 import net.sunken.common.DataManager;
 import net.sunken.common.packet.PacketHandler;
 import net.sunken.common.player.packet.PlayerQuitPacket;
+import net.sunken.master.Master;
+import net.sunken.master.friend.FriendManager;
 
 public class PlayerQuitHandler extends PacketHandler<PlayerQuitPacket> {
 
     @Override
     public void onReceive(PlayerQuitPacket packet) {
+        // Remove from player cache
         DataManager dataManager = Common.getInstance().getDataManager();
         dataManager.getOnlinePlayers()
                 .remove(packet.getUuid());
         dataManager.getNameToUUID()
                 .remove(packet.getName().toLowerCase());
+
+        // Remove pending friend invites
+        FriendManager friendManager = Master.getInstance().getFriendManager();
+        friendManager.getFriendInvites().removeAll(packet.getUuid());
     }
 
 }
