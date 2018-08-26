2bf8942b849f7374562d0399ef25bf1ec1d20113
Digital <josephpro42@live.co.uk>
2018-08-26 21:48:34 +0100

    Check if target server can fit players on summon

--- master/src/main/java/net/sunken/master/party/PartySummonHandler.java
+++ master/src/main/java/net/sunken/master/party/PartySummonHandler.java
@@ -1,5 +1,7 @@
 package net.sunken.master.party;
 
+import net.sunken.common.Common;
+import net.sunken.common.DataManager;
 import net.sunken.common.packet.PacketHandler;
 import net.sunken.common.packet.PacketUtil;
 import net.sunken.common.parties.data.Party;
@@ -8,12 +10,18 @@ import net.sunken.common.parties.packet.MustBeInPartyPacket;
 import net.sunken.common.parties.packet.MustBeLeaderPacket;
 import net.sunken.common.parties.packet.changes.PartyAttemptSummonPacket;
 import net.sunken.common.parties.packet.request.MPartySummonRequestPacket;
+import net.sunken.common.server.ServerObjectCache;
+import net.sunken.common.server.data.ServerObject;
+import net.sunken.master.player.MasterPlayer;
 
 import java.util.Set;
 import java.util.UUID;
 
 /** Requests for summoning players to a server are handled here. */
 public class PartySummonHandler extends PacketHandler<MPartySummonRequestPacket> {
+
+    private static final DataManager dataManager = Common.getInstance().getDataManager();
+    private static final ServerObjectCache serverCache = Common.getInstance().getServerCache();
 
     @Override
     public void onReceive(MPartySummonRequestPacket packet) {
@@ -33,6 +41,31 @@ public class PartySummonHandler extends PacketHandler<MPartySummonRequestPacket>
         }
 
         Set<PartyPlayer> allMembers = party.getAllMembers();
+
+        // Calculate online members
+        int onlineMembers = 0;
+        for (PartyPlayer player : allMembers) {
+            if (dataManager.getOnlinePlayers().containsKey(player.getUniqueId())) {
+                onlineMembers++;
+            }
+        }
+
+        // Get server by name
+        ServerObject server = null;
+        for (ServerObject serv : serverCache.getCache()) {
+            if (serv.getServerName().equals(serverName)) {
+                server = serv;
+            }
+        }
+
+        // Checking if the server cannot fit the party, removing the one because
+        // of leader is already on the target server. max - count = slots available.
+        if ((server.getMaxPlayers() - server.getPlayerCount()) < (onlineMembers - 1)) {
+            PacketUtil.sendPacket(new MustBeLeaderPacket(
+                    requester, "Unable to summon party, theres not enough room on your server!"));
+            return;
+        }
+
         PartyAttemptSummonPacket partyAttemptSummonPacket = new PartyAttemptSummonPacket(
                 serverName,
                 requester,
